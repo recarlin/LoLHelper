071a4e16466aca01bc23a85e0a4be1bd5b509523
Russell Carlin <recarlin@fullsail.com>
2012-04-03 20:43:25 -0400

    Things coming together. Working on reading xml and csv files.

--- JS/main.js
+++ JS/main.js
@@ -1,67 +1,25 @@
 // Russell Carlin
 // ASD 1204
-// Project 1
+// Project 2
 
-$(document).ready(function(){
+$('#home').live('pageinit', function(){
 
 	$.mobile.page.prototype.options.addBackBtn= true;
 
-    $('#clickTop').click(function(){
+    $('#thumbnails div').on('click', function(){
      window.location=$(this).find('a').attr('href');
      return false;
     });
     
-    $('#clickMid').click(function(){
-     window.location=$(this).find('a').attr('href');
-     return false;
-    });
-    
-    $('#clickBot').click(function(){
-     window.location=$(this).find('a').attr('href');
-     return false;
-    });
-    
-    $('#clickJungle').click(function(){
-     window.location=$(this).find('a').attr('href');
-     return false;
-    });
-    
-    $('#gGame').change(function() {
-    	var x= $('#gGame').val()
-    	$('#gType option').not('#default').remove()
-        for(index in types[x]) {
-        	$('#gType').append('<option value="' + types[x][index] + '">' + types[x][index] + '</option>')
-        };
-    	$('#gType').selectmenu('refresh', true);
-    });
-    
-    $('#gGuideForm').validate({
-    	invalidHandler: function(form, validator){},
-    	submitHandler: function(){
-    		alert('Guide saved!');
-    		location.reload();
-    	}
-    });
-    
-    $('#gSlide').hide();
-    
-    $('#gELO').click(function(){
-	    if ($('#gELO').is(':checked')){
-	    	$('#gSlide').show()
-    	} else {
-			$('#gSlide').hide()
-		};
-    });
-    
-    $('#browser').bind('pagebeforeshow', function(){
-	    $.getJSON('JS/filler.json', function(data){
+    $('#browser').on('pageinit', function(){
+	    $.getJSON('externalData/roster.json', function(data){
 			$.each(data, function(id, info){
-				$('<ul/>', {html: id + ': ', 'data-role':'listview'}).appendTo('#display');
+				$('<ul/>', {html: id + ': '}).appendTo('#display');
 				$.each(info, function(key, value){
 					$('<li/>', {html: key + ": " + value}).appendTo('#display ul:last');
 				});
 			});
 		});
-		$('#display ul').listview('refresh');
+		$('#display').listview('refresh');
 	});
 });
